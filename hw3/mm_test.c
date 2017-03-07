95674b1e0d70512ef3cdd8d95aec8099434d0f59
byMyWay <orglanss@gmail.com>
2017-03-07 08:39:55 +0000

    finished hw3

--- hw3/mm_test.c
+++ hw3/mm_test.c
@@ -7,7 +7,7 @@
 void* (*mm_malloc)(size_t);
 void* (*mm_realloc)(void*, size_t);
 void (*mm_free)(void*);
-
+void (*mm_info)();
 void load_alloc_functions() {
     void *handle = dlopen("hw3lib.so", RTLD_NOW);
     if (!handle) {
@@ -33,14 +33,43 @@ void load_alloc_functions() {
         fprintf(stderr, "%s\n", dlerror());
         exit(1);
     }
+    mm_info = dlsym(handle, "mm_info");
+    if ((error = dlerror()) != NULL)  {
+        fprintf(stderr, "%s\n", dlerror());
+        exit(1);
+    }
 }
 
 int main() {
     load_alloc_functions();
 
-    int *data = (int*) mm_malloc(sizeof(int));
+    int *data = (int *)mm_malloc(sizeof(int));   
+
+    // void *p[100];
+    // int j = 0;
+    // for (int i = 0; i < 100; i += 4,j++)
+    // {
+    //     p[j] = mm_malloc(i);
+    // }
+    
+    // mm_free(p[14]);
+    // mm_free(p[16]);
+    // mm_free(p[18]);
+    // mm_free(p[15]);
+    // mm_free(p[17]);
+    // mm_info();
+    // strcpy(p[14],"This is a test");
+    // char *np = mm_realloc(p[14], 4);
+    // printf("%s\n", np);}
+    void *t;
+    for (int i = 0; i < 10; ++i)
+    {
+        t = malloc(i);
+        printf("%p\n",t);
+    }
     assert(data != NULL);
-    data[0] = 0x162;
+
+    //data[0] = 0x162;
     mm_free(data);
     printf("malloc test successful!\n");
     return 0;
